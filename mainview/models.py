500e2eccace85328c09e2aa9e5d75bf57b555c82
Michael Roos <michael.d.roos@gmail.com>
2018-04-02 20:16:53 -0400

    Initial API commit

---
+++ mainview/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.postgres.fields import JSONField
+
+
+# Create your models here.
+class event (models.Model):
+	id = models.IntegerField(primary_key=True)
+	title = models.CharField(max_length=20)
+	description = models.CharField(max_length=160)
+	location = models.CharField(max_length=20)
+	user_email = models.CharField(max_length=160)
+	time = models.DateTimeField()
+	comments = JSONField()
+
+
+	def __str__(self):
+		return self.title
+
+
+class times (models.Model):
+	start_time = models.DateTimeField()
+	end_time = models.DateTimeField()
+	duration = models.IntegerField()
+	event_id = models.ForeignKey(event)
+
+	class Meta:
+		unique_together = ("start_time", "end_time", "event_id")
+
