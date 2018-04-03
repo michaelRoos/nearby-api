47d87d2828a559f1d81486c77617c9752174fdec
Lawrence Lee <lfqlee314@gmail.com>
2018-04-02 22:03:34 -0400

    fix network settings with cors and allowed hosts

--- Nearby/settings.py
+++ Nearby/settings.py
@@ -23,12 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rzzoe)b=@=+=$)j-vjxmrtky2po=5w*(qhomn7%hd9r#el&i53'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-18-188-184-129.us-east-2.compute.amazonaws.com', 'localhost', '127.0.0.1']
 
-CORS_ORIGIN_WHITELIST = False
-
+CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
 
 INSTALLED_APPS = [
