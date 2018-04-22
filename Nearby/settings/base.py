4c1979451c44fda49bb5710a7b97f46f7d70a9ab
Lawrence Lee <lfqlee314@gmail.com>
2018-04-22 17:56:05 -0400

    change jwt expire time to 1 day

--- Nearby/settings/base.py
+++ Nearby/settings/base.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import datetime
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -92,6 +94,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Nearby.wsgi.application'
 
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=24),
+}
 
 
 
